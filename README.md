79506c29025694bfca78d7fe6068e00bbb93e18b
Julie Rindone <julie.rindone@gmail.com>
2026-02-14 15:28:10 -0800

    Initial commit (after copying lorem-smithsum code & making a few changes)

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Smiths Valentine Generator
+### _a valentine generator, but it's human and it needs to be loved_<br><br>
+
+
+
+
+
---
+++ data.js
@@ -0,0 +1,69 @@
+export const quotes = [
+	{
+		lineOne: `It's not like any other love`,
+		lineTwo: `This one is different, because it's us`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `The good life is out there somewhere`,
+		lineTwo: `So stay on my arm, you little charmer`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `I'd lik to drop my trousers to the world.`,
+		lineTwo: `(<em>You</em> are my world, Valentine.)`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `I am writing this to say,<br>In a gentle way`,
+		lineTwo: `<em>Thank you, but no.</em>`,
+		artist: `Morrissey`
+	},
+	/*
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: ``
+	},
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: ``
+	},
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: ``
+	},
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: ``
+	},
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: ``
+	},
+	{
+		lineOne: ``,
+		lineTwo: ``,
+		artist: ``
+	},
+*/
+]
