4c2bffddd72a291f680c24052e3c7e86a9bbef2b
Julie Rindone <julie.rindone@gmail.com>
2026-02-14 22:51:12 -0800

    Added more quotes

--- data.js
+++ data.js
@@ -20,50 +20,101 @@ export const quotes = [
 		artist: `Morrissey`
 	},
 	{
-		lineOne: `You're the one for me, fatty`,
+		lineOne: `You're the one for me, fatty!`,
 		lineTwo: `You're the one I really, really love!`,
 		artist: `Morrissey`
 	},
-	/*
 	{
-		lineOne: ``,
-		lineTwo: ``,
+		lineOne: `Heavy words are so lightly thrown,`,
+		lineTwo: `But still I'd leap in front of a flying bullet for you.`,
 		artist: `The Smiths`
 	},
 	{
-		lineOne: ``,
-		lineTwo: ``,
+		lineOne: `Last night, I dreamt that somebody loved me`,
+		lineTwo: `No hope, no harm, Just another false alarm.`,
 		artist: `The Smiths`
 	},
 	{
-		lineOne: ``,
-		lineTwo: ``,
+		lineOne: `You're not right in the head, and nor am I,`,
+		lineTwo: `And this is why I like you.`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `I dreamt about you last night,`,
+		lineTwo: `And I fell out of bed twice.`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `You're the bee's knees,`,
+		lineTwo: `But so am I.`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `<span style="font-size: 1.25em">Valentine,</span>`,
+		lineTwo: `You can pin and mount me like a butterfly!`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `Fifteen minutes with you?`,
+		lineTwo: `Well, I wouldn't say no!`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `I know I'm unloveable.`,
+		lineTwo: `You don't have to tell me.`,
 		artist: ``
 	},
 	{
-		lineOne: ``,
-		lineTwo: ``,
-		artist: ``
+		lineOne: `Oh, but don't mention love,`,
+		lineTwo: `I'd hate the strain of the pain again.`,
+		artist: `The Smiths`
 	},
 	{
-		lineOne: ``,
-		lineTwo: ``,
-		artist: ``
+		lineOne: `And if a double-decker bus crashes into us,`,
+		lineTwo: `To die by your side, is such a heavenly way to die!`,
+		artist: `The Smiths`
 	},
 	{
-		lineOne: ``,
-		lineTwo: ``,
-		artist: ``
+		lineOne: `Sweetness, I was only joking when I said`,
+		lineTwo: `I'd like to smash every tooth in you head!`,
+		artist: `The Smiths`
 	},
 	{
-		lineOne: ``,
-		lineTwo: ``,
-		artist: ``
+		lineOne: `I don't mind...`,
+		lineTwo: `I don't mind if you forget me.`,
+		artist: `Morrissey`
 	},
 	{
-		lineOne: ``,
-		lineTwo: ``,
-		artist: ``
+		lineOne: `Love is natural and real,`,
+		lineTwo: `But not for you and I, my love.`,
+		artist: `The Smiths`
 	},
-*/
+	{
+		lineOne: `It's so easy to laugh, it's so easy to hate,`,
+		lineTwo: `It takes strength to be gentle and kind.`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `Time's tide will smother you`,
+		lineTwo: `And I will too, Valentine!`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `Scratch my name on your arm with a fountain pen.`,
+		lineTwo: `(This means you really love me.)`,
+		artist: `The Smiths`
+	},
+	{
+		lineOne: `Please help the cause`,
+		lineTwo: `against loneliness!`,
+		artist: `Morrissey`
+	},
 ]
+
+/*
+{
+	lineOne: ``,
+	lineTwo: ``,
+	artist: `The Smiths`
+},
+*/
